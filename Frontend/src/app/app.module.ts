2eae16843dcff7f772e886004209ef932380c932
flortalavera <florii.talavera@gmail.com>
2022-03-29 16:44:08 -0300

    login

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+
+import { LoginService } from './services/login.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    LoginService,
+    CookieService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ Frontend/src/app/components/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../../services/login.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['../../../../css/login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  username = "";
+  password = "";
+
+  constructor(
+    private loginService: LoginService,
+    public router: Router
+  ) { }
+
+  ngOnInit(): void {
+    
+  }
+
+  // Login clásico
+
+  login() {
+    const user = { username: this.username, password: this.password };
+    this.loginService.login(user).subscribe((data: any) => {
+      this.loginService.setToken(data.token);
+      this.router.navigateByUrl('/');
+    });
+  }
+
+}
