391d7dd1da33936cac352a3c36b2330ed2a58676
Nazareno Gabriel Bastanzo <b.nazarenogabriel@gmail.com>
2022-04-03 12:54:00 -0300

    Angular Material Added

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CrearCuentaComponent } from './components/crear-cuenta/crear-cuenta.component';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, CrearCuentaComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule, FormsModule, BrowserAnimationsModule],
   providers: [Title],
   bootstrap: [AppComponent],
 })
