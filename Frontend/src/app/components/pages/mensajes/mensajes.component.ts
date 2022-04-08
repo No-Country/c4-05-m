2a221d6cfae21cf9c348b9af15ea0d16adcd7fb6
Nazareno Gabriel Bastanzo <b.nazarenogabriel@gmail.com>
2022-04-08 16:10:12 -0300

    Funcionamiento de mensajes

--- Frontend/src/app/components/pages/mensajes/mensajes.component.ts
+++ Frontend/src/app/components/pages/mensajes/mensajes.component.ts
@@ -1,21 +1,29 @@
+import { HttpConfigService } from './../../../services/http-config.service';
+import { LoginService } from './../../../services/login.service';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-mensajes',
   templateUrl: './mensajes.component.html',
-  styleUrls: ['./mensajes.component.css']
+  styleUrls: ['./mensajes.component.css'],
 })
 export class MensajesComponent implements OnInit {
-
   @ViewChild('messagesBox')
   private myScrollContainer!: ElementRef;
+  @ViewChild('inputMessage') private theInputMessage!: ElementRef;
 
   viewEmojis: boolean;
   message: string;
+  user: any;
+  outcomingMessages: any[] = [];
+  userImg!: string;
 
   constructor(
-    private router: Router
+    private router: Router,
+    private loginService: LoginService,
+    private httpService: HttpConfigService
   ) {
     this.viewEmojis = false;
     this.message = '';
@@ -23,6 +31,18 @@ export class MensajesComponent implements OnInit {
 
   ngOnInit() {
     this.scrollToBottom();
+
+    const userId = this.loginService.getUserId();
+
+    this.httpService
+      .get<any>(`${environment.apiUrl}/user/${userId}`, true)
+      .subscribe({
+        next: (resp: any) => {
+          this.user = resp.data.user;
+        },
+        error: (error) => {},
+        complete: () => {},
+      });
   }
 
   ngAfterViewChecked() {
@@ -30,9 +50,10 @@ export class MensajesComponent implements OnInit {
   }
 
   scrollToBottom(): void {
-      try {
-          this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
-      } catch(err) { }
+    try {
+      this.myScrollContainer.nativeElement.scrollTop =
+        this.myScrollContainer.nativeElement.scrollHeight;
+    } catch (err) {}
   }
 
   goHome() {
@@ -40,11 +61,31 @@ export class MensajesComponent implements OnInit {
   }
 
   addEmoji(event: any) {
-
     this.message += event.emoji.native;
 
     this.viewEmojis = false;
-
   }
 
+  sendMessage(message: string) {
+
+    const userId = this.loginService.getUserId();
+
+    this.httpService
+      .get<any>(`${environment.apiUrl}/user/${userId}`, true)
+      .subscribe({
+        next: (resp: any) => {
+
+          this.userImg = resp.data.user.img;
+          const theMessage = {
+            message,
+            userImg: this.userImg,
+          };
+
+          this.outcomingMessages.push(theMessage);
+          this.theInputMessage.nativeElement.value = '';
+        },
+        error: (error) => {},
+        complete: () => {},
+      });
+  }
 }
