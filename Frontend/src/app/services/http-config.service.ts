c1a11cb99773ec1a7aa13315ca889bc03a1eae65
Nazareno Gabriel Bastanzo <b.nazarenogabriel@gmail.com>
2022-04-11 00:54:24 -0300

    small fix & formatting

--- Frontend/src/app/services/http-config.service.ts
+++ Frontend/src/app/services/http-config.service.ts
@@ -4,30 +4,40 @@ import { Observable } from 'rxjs';
 import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HttpConfigService {
-
   private _headers!: HttpHeaders;
 
-  constructor(
-    private http: HttpClient,
-    private cookies: CookieService
-  ) {
+  constructor(private http: HttpClient, private cookies: CookieService) {
     // Aquí se setean los headers
     this._headers = new HttpHeaders({
-      'Content-Type':  'application/json',
-      'Authorization': 'Bearer ' + this.cookies.get('token')
+      'Content-Type': 'application/json',
+      Authorization:
+        'Bearer ' + this.cookies.check('token')
+          ? this.cookies.get('token')
+          : this.cookies.get('emailToken'),
     });
   }
 
   // Función genérica para realizar cualquier petición POST
-  public post<T>(url: string, data: any, activateHeader: boolean = false): Observable<T> {
-    return this.http.post<T>(url, data, activateHeader ? { headers: this._headers }: {});
+  public post<T>(
+    url: string,
+    data: any,
+    activateHeader: boolean = false
+  ): Observable<T> {
+    return this.http.post<T>(
+      url,
+      data,
+      activateHeader ? { headers: this._headers } : {}
+    );
   }
 
   // Función genérica para realizar cualquier petición GET
   public get<T>(url: string, activateHeader: boolean = false): Observable<T> {
-    return this.http.get<T>(url, activateHeader ? { headers: this._headers }: {});
+    return this.http.get<T>(
+      url,
+      activateHeader ? { headers: this._headers } : {}
+    );
   }
 }
